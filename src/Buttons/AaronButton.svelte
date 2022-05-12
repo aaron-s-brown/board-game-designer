73a22bcbc564258f3c5fc4278b939e692f99ab4e
Aaron Brown <aaron@osmos.io>
2022-05-11 22:08:49 -0700

    Aaron button

---
+++ src/Buttons/AaronButton.svelte
@@ -0,0 +1,12 @@
+<script>
+	let count = 1;
+
+	function incrementCount() {
+		// event handler code goes here
+		count *= 2;
+	}
+</script>
+
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
