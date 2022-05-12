104f080e1f4f0548b5a8ca16ff1c0f7d592c92de
Victor! <victor88@uw.edu>
2022-05-11 22:41:23 -0700

    beer me.

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello You 🍺{name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<AaronButton/>
 	Hello
